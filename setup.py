50c11ca24d206ba37285e57c5c0738c6a361236e
Kalle Bracht <kalle.bracht@outlook.de>
2021-01-26 14:31:33 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='ae_python',
+    version='1.0.0',
+    author='Kalle Bracht',
+    author_email='kalle.bracht@outlook.de',
+    packages=["ae_python"],
+    url="https://kalbra.github.io/after-effects-python/index.html",
+    license='LICENSE',
+    description='Create After Effects scripts in Python.',
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "colour",
+        "secrets",
+    ],
+)
